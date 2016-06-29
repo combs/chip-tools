58eb0718ecb99e037cda37e2c0fb0402c556e7d6
Chris Combs <ccombs@gmail.com>
2016-06-28 21:31:22 -0400

    less garbo

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 FILENAME=`hostname`-backup.`date +%Y%m%d`.tgz
-tar czf /$FILENAME / --exclude=/$FILENAME --exclude=/proc --exclude=/sys
+tar czf /$FILENAME / --exclude=/\*.tgz --exclude=/proc --exclude=/sys --exclude=/run 2>/dev/null
 scp /$FILENAME combs@batman.local:/Volumes/Foton/Backups/ && rm /$FILENAME
