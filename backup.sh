58a5b7e0d43bc7ca952b692ee0f9d3fb840b0455
combs <ccombs@gmail.com>
2016-08-14 00:19:14 -0400

    fix location

--- backup.sh
+++ backup.sh
@@ -3,5 +3,5 @@ BASENAME=`hostname`-backup
 FILENAME=$BASENAME.`date +%Y%m%d`.tgz
 tar czf /$FILENAME / --exclude=/\*.tgz --exclude=/proc --exclude=/sys --exclude=/run 2>/dev/null
 for arg in /$BASENAME*
-	do scp "$arg" combs@batman.local:/Volumes/Foton/Backups/ && rm "$arg"
+	do scp "$arg" combs@batman.local:/Users/combs/Dropbox/Targets/Foton-Backups/ && rm "$arg"
 done
