e6c51dd66b7d0af4f336bca83d527dbcdc7e3927
combs <ccombs@gmail.com>
2016-08-20 21:31:17 -0400

    functions

--- switches.py
+++ switches.py
@@ -1,21 +1,24 @@
 import CHIP_IO.GPIO as GPIO
 import fb
 
-GPIO.setup("U14_13", GPIO.IN)
-GPIO.setup("U14_14", GPIO.IN)
-GPIO.setup("U14_15", GPIO.IN)
-GPIO.setup("U14_16", GPIO.IN)
-GPIO.setup("U14_17", GPIO.IN)
+def switches_setup():
+	GPIO.setup("U14_13", GPIO.IN)
+	GPIO.setup("U14_14", GPIO.IN)
+	GPIO.setup("U14_15", GPIO.IN)
+	GPIO.setup("U14_16", GPIO.IN)
+	GPIO.setup("U14_17", GPIO.IN)
 
-value = 0
+def switches_read():
+	value = 0
+	if GPIO.input("U14_13"):
+		value = value + 1
+	if GPIO.input("U14_14"):
+		value = value + 2
+	if GPIO.input("U14_15"):
+		value = value + 4
+	if GPIO.input("U14_16"):
+		value = value + 8
+	return value
 
-if GPIO.input("U14_13"):
-	value = value + 1 
-if GPIO.input("U14_14"):
-	value = value + 2 
-if GPIO.input("U14_15"):
-	value = value + 4 
-if GPIO.input("U14_16"):
-	value = value + 8 
-
-print(value)
+switches_setup()
+print(switches_read())
