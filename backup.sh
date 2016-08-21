b52eec881f0fb78bf22c7263296d3b42245e69ba
combs <ccombs@gmail.com>
2016-08-20 20:52:35 -0400

    clean apt cache

--- backup.sh
+++ backup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 BASENAME=`hostname`-backup
 FILENAME=$BASENAME.`date +%Y%m%d`.tgz
+
+apt-get clean
 tar czf /$FILENAME / --exclude=/\*.tgz --exclude=/proc --exclude=/sys --exclude=/run 2>/dev/null
 for arg in /$BASENAME*
 	do scp "$arg" combs@batman.local:/Users/combs/Dropbox/Targets/Foton-Backups/ && rm "$arg"
